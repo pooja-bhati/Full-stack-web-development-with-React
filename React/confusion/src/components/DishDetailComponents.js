4ddcfcf17276e82b5b8ea9922a32afda50a56df4
Pooja Bhati <bhatipooja78@gmail.com>
2020-07-25 15:00:48 +0530

    Single Page Applications Part 2

--- React/confusion/src/components/DishDetailComponents.js
+++ React/confusion/src/components/DishDetailComponents.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
 
@@ -57,10 +58,20 @@ function RenderDish({dish}) {
         else {
           return (
               <div class= "container">
-              <div className='row'>
-                  <RenderDish dish={props.dish} />
-                  <RenderComments comments={props.dish.comments} />
-              </div>
+                <div className="row">
+                      <Breadcrumb>
+                          <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                      </Breadcrumb>
+                      <div className="col-12">
+                          <h3>{props.dish.name}</h3>
+                          <hr />
+                      </div>
+                  </div>
+                <div className='row'>
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
+                </div>
               </div>
           );
     }
