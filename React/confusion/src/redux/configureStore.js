7a03c651bf0f441b8ceb2030fc343a9464e07ce0
Pooja Bhati <bhatipooja78@gmail.com>
2020-08-09 15:52:32 +0530

    react redux form revisited

--- React/confusion/src/redux/configureStore.js
+++ React/confusion/src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes} from './dishes';
 import { Comments} from './comments';
 import { Promotions} from './promotions';
 import { Leaders} from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './form';
 
 
 export const ConfigureStore = () => {
@@ -13,8 +15,11 @@ export const ConfigureStore = () => {
         dishes: Dishes,
         comments: Comments,
         promotions: Promotions,
-        leaders: Leaders
-      }),
+        leaders: Leaders,
+        ...createForms({
+                        feedback: InitialFeedback
+                    })
+        }),
         applyMiddleware(thunk, logger)
     );
 
